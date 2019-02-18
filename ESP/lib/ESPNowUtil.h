738cb030fad1013eba8ec77015627fa8dcc3befb
Scott Zhao <zhaomh1998@outlook.com>
2019-02-18 02:32:19 -0800

    eagle library update, code repo restructure

---
+++ ESP/lib/ESPNowUtil.h
@@ -0,0 +1,12 @@
+//
+// Created by Scott Zhao on 2019-02-17.
+//
+
+#ifndef ESP_ESPNOWUTIL_H
+#define ESP_ESPNOWUTIL_H
+
+#include <ESP8266WiFi.h>
+class ESPNowUtil {
+
+};
+#endif //ESP_ESPNOWUTIL_H
